50ff21cbc4cc2a33d6507308fca1dfb8793a21f8
Cesar cardenas <dev@cesar.solutions>
2023-02-09 18:51:44 -0600

    create hook for typing animation

--- src/hooks/useTyping.tsx
+++ src/hooks/useTyping.tsx
@@ -1,18 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 type Props = {
-    text:string;
-    typeSpeed?:number;
+    text: string;
+    typeSpeed?: number;
 
 }
 
 const useTyping = (props: Props) => {
-    const {text,typeSpeed=200}=props
+    const { text, typeSpeed = 200 } = props
     const [textTyping, setTextTyping] = useState('')
 
-    
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setTextTyping((prev: string) => {
+                if (prev === text) {
+                    clearInterval(interval)
+                    return prev
+                }
+                else {
+                    return prev + text[prev.length]
+                }
+
+            })
+        }, typeSpeed)
 
 
+
+        return () => {
+        }
+    }, [])
+
+    return { textTyping }
 
 
 }
